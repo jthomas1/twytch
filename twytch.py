744f9791a9a74d428d842c2c1d429a35d6ea2793
James <plinkplonk@gmail.com>
2016-02-15 20:43:48 +0000

    add limit for only 1 result

--- twytch.py
+++ twytch.py
@@ -6,14 +6,12 @@ import requests
 if len(sys.argv) > 1:
     address = ' '.join(sys.argv[1:])
 else:
-    url = "https://api.twitch.tv/kraken/streams?game=Counter-Strike:+Global+Offensive"
+    url = "https://api.twitch.tv/kraken/streams?game=Counter-Strike:+Global+Offensive&limit=1"
     response = requests.get(url)
 
     if(response.status_code == 200):
         channels = response.json()
-        urls = []
         for item in channels["streams"]:
-          urls.append(item["channel"]["url"])
-        address  = urls[0]
+            address = item["channel"]["url"]
 
 os.system("livestreamer " + address + " source")

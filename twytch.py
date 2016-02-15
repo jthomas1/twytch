b3b48adcb76e962c0ef2dae2e9871cf8a0fbbe7d
James <plinkplonk@gmail.com>
2016-02-15 20:53:08 +0000

    removed limit, provide list to user to choose by index

--- twytch.py
+++ twytch.py
@@ -6,12 +6,17 @@ import requests
 if len(sys.argv) > 1:
     address = ' '.join(sys.argv[1:])
 else:
-    url = "https://api.twitch.tv/kraken/streams?game=Counter-Strike:+Global+Offensive&limit=1"
+    url = "https://api.twitch.tv/kraken/streams?game=Counter-Strike:+Global+Offensive"
     response = requests.get(url)
 
     if(response.status_code == 200):
         channels = response.json()
-        for item in channels["streams"]:
-            address = item["channel"]["url"]
+        urls = []
+        for key, item in enumerate(channels["streams"]):
+            url = item["channel"]["url"]
+            print(str(key) + ": " + url)
+            urls.append(url)
+        choice = input("Pick a number: ")
+        address = (urls[int(choice)])
 
 os.system("livestreamer " + address + " source")

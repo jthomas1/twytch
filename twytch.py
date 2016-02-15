a2035c316edfddbf501b3476c992df2d10eb2131
James <plinkplonk@gmail.com>
2016-02-15 20:33:55 +0000

    refactor
    if url provided as arg go there otherwise get top urls and open the
    first one (for now, do something more sophistcated with the results
    later)

--- twytch.py
+++ twytch.py
@@ -1,23 +1,19 @@
 import os
 import sys
-import pyperclip
 import requests
 
 
 if len(sys.argv) > 1:
     address = ' '.join(sys.argv[1:])
 else:
-    address = pyperclip.paste()
+    url = "https://api.twitch.tv/kraken/streams?game=Counter-Strike:+Global+Offensive"
+    response = requests.get(url)
 
-url = "https://api.twitch.tv/kraken/streams?game=Counter-Strike:+Global+Offensive"
+    if(response.status_code == 200):
+        channels = response.json()
+        urls = []
+        for item in channels["streams"]:
+          urls.append(item["channel"]["url"])
+        address  = urls[0]
 
-response = requests.get(url)
-
-if(response.status_code == 200):
-    channels = response.json()
-    urls = []
-    for item in channels["streams"]:
-        urls.append(item["channel"]["url"])
-    os.system("livestreamer " + urls[0] + " source")
-else:
-    print("No response")
+os.system("livestreamer " + address + " source")

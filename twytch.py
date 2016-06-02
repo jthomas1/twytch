c8056defe2caf8f4e803cb9287394d2177f58c9c
James <plinkplonk@gmail.com>
2016-06-02 23:51:23 +0100

    argparse refactor complete
    Argparse action implemented to allow urls to be retrieved from clipboard
    if not specified as arguments.

--- twytch.py
+++ twytch.py
@@ -1,5 +1,4 @@
 import os
-import sys
 import argparse
 import requests
 import shutil
@@ -7,19 +6,15 @@ import webbrowser
 import pyperclip
 
 
-help_text = """
-        Usage:
-            -u followed by a valid twitch url for livestream
-            -p followed by a valid twitch past broadcast url
-            --csgo for a list of top csgo streams
-    """
-
-csgo_uri = "https://api.twitch.tv/kraken/streams?game=" \
-           "Counter-Strike:+Global+Offensive"
+class GetUrlAction(argparse.Action):
+    def __call__(self, parser, namespace, values, option_string=None):
+        if values is 'clip':
+            values = pyperclip.paste()
+        setattr(namespace, self.dest, values)
 
 
-def usage():
-    print(help_text)
+def out(str):
+    print("<-twytch-> {}".format(str))
 
 
 def check_twitch_url(url):
@@ -30,10 +25,12 @@ def check_twitch_url(url):
 
 
 def invalid_url(url):
-    print("Invalid url: {}".format(url))
+    out("Invalid url: {}".format(url))
 
 
 def get_top_streams():
+    csgo_uri = "https://api.twitch.tv/kraken/streams?game=" \
+               "Counter-Strike:+Global+Offensive"
     response = requests.get(csgo_uri)
     if response.status_code == requests.codes.ok:
         channels = response.json()
@@ -41,10 +38,22 @@ def get_top_streams():
         for key, item in enumerate(channels["streams"]):
             url = item["channel"]["url"]
             name = item["channel"]["display_name"]
-            print("{}: {} - {}".format(str(key), name, url))
+            out("{}: {} - {}".format(str(key), name, url))
             urls.append(url)
         choice = input("Pick a number: ")
         return urls[int(choice)]
+
+
+def launch_stream(url, is_past_broadcast):
+    if shutil.which('livestreamer') is not None:
+        out("livestreamer available, launching...")
+        cmd_str = "livestreamer " + url + " source "
+        if(is_past_broadcast):
+            cmd_str += " --player-passthrough hls"
+        os.system(cmd_str)
+    else:
+        out("livestreamer unavailable, falling back to browser")
+        webbrowser.open(url)
 
 
 def main():
@@ -53,56 +62,49 @@ def main():
     parser.add_argument(
         'url',
         help='Load twitch live stream by URL',
+        type=str,
+        const="clip",
+        action=GetUrlAction,
         nargs='?')
     parser.add_argument(
         '-p',
         help='Load twitch past broadcast by URL',
+        type=str,
         const="clip",
+        action=GetUrlAction,
         nargs='?')
+    parser.add_argument(
+        '-cs',
+        help='Show list of top CS:GO stream',
+        action="store_true")
 
     args = parser.parse_args()
-    print(args)
-
     past_broadcast = False
+    url = None
 
-    if args.url is not None:
-        # use livestream url provided
-        if check_twitch_url(args.url):
-            url = args.url
-        else:
-            invalid_url(args.url)
-    elif args.url is None and args.p is None:
-        # get live stream url from pyperclip
-        if check_twitch_url(args.url):
-            url = pyperclip.paste()
-        else:
-            invalid_url(args.url)
-    elif args.url is None and args.p is "clip":
-        # get live stream url from pyperclip
-        if check_twitch_url(args.p):
-            url = pyperclip.paste()
-            past_broadcast = True
-        else:
-            invalid_url(args.p)
+    if args.cs is True:
+        out("Loading top CS:GO streams")
+        get_top_streams()
     elif args.p is not None:
-        # use past broadcast url provided
+        out("Loading past broadcast")
         if check_twitch_url(args.p):
             url = args.p
             past_broadcast = True
         else:
             invalid_url(args.p)
+    elif args.url is not None:
+        out("Loading live stream")
+        if check_twitch_url(args.url):
+            url = args.url
+        else:
+            invalid_url(args.url)
     else:
-        print(":(")
+        out("Something went wrong :(")
 
-    if shutil.which('livestreamer') is not None:
-        print("livestreamer available, launching...")
-        cmd_str = "livestreamer " + url + " source "
-        if(past_broadcast):
-            cmd_str += " --player-passthrough hls"
-        os.system(cmd_str)
+    if url is not None:
+        launch_stream(url, past_broadcast)
     else:
-        print("livestreamer unavailable, falling back to browser")
-        webbrowser.open(url)
+        out("No url :(")
 
 
 if __name__ == '__main__':
